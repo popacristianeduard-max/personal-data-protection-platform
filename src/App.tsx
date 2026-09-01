0d1950196dde767d6d5fc75b87ace13eda7b31d5
popacristianeduard-max <popacristianeduard@gmail.com>
2026-09-01 06:52:50 +0300

    Add some files to include in this commit.

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,9 @@ import { LocaleProvider } from "./LocaleContext"
 
 function ScrollToTop() {
   const { pathname } = useLocation()
-  useEffect(() => window.scrollTo(0, 0), [pathname])
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
   return null
 }
 
